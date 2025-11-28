2bc139ba7d1bc75d0a937f8c654cdaa3929eeed2
Akif-jpg <akifesadi193@gmail.com>
2025-11-28 20:05:49 +0300

    First commit

---
+++ p2p-simulation/src/main/java/com/gidp/p2psimulation/App.java
@@ -0,0 +1,13 @@
+package com.gidp.p2psimulation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
